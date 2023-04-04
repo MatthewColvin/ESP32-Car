fd1d47fa98cb41024f404c3cbb201d295d5944b1
MatthewColvin <mc067415@ohio.edu>
2023-04-03 19:59:19 -0500

    add initial motor class for car planning on using mcpwm for the control of the motors.

---
+++ components/Car/motor.cpp
@@ -0,0 +1,24 @@
+#include "motor.hpp"
+
+// const byte rightMotorAPin = 15;
+// const byte rightMotorBPin = 2;
+// const byte leftMotorAPin = 16;
+// const byte leftMotorBPin = 17;
+
+// const byte rightMotorAChannel = 0;
+// const byte rightMotorBChannel = 1;
+// const byte leftMotorAChannel = 2;
+// const byte leftMotorBChannel = 3;
+
+// const byte redLED = 19;
+// const byte greenLED = 18;
+// const byte blueLED = 5;
+
+// const byte servoPin = 13;
+
+// const int freq = 500;     // Set timer to run at 500Hz
+// const int resolution = 8; // Set timer to 8-bit
+
+Motor::Motor(int pin)
+{
+}
