0a263d8840fa699cfd1ff608d1f14d90071a981c
MatthewColvin <mc067415@ohio.edu>
2023-04-04 21:42:01 -0500

    added 3rd party deps for motor control and working to flush out motor class possibly with pid control.
    will need to call idf.py reconfigure to fetch deps which will be downloaded into managed components

--- components/Car/include/motor.hpp
+++ components/Car/include/motor.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
+#include "bdc_motor.h"
+
 class Motor
 {
-    Motor(int pin);
+public:
+    Motor(const int LeftPin, const int rightPin);
+
+private:
+    bdc_motor_config_t mConfig;
+    bdc_motor_mcpwm_config_t mPwmConfig;
+
+    bdc_motor_handle_t mHandle;
 };
--- components/Car/motor.cpp
+++ components/Car/motor.cpp
@@ -1,4 +1,5 @@
 #include "motor.hpp"
+#include "bdc_motor.h"
 
 // const byte rightMotorAPin = 15;
 // const byte rightMotorBPin = 2;
@@ -19,6 +20,18 @@
 // const int freq = 500;     // Set timer to run at 500Hz
 // const int resolution = 8; // Set timer to 8-bit
 
-Motor::Motor(int pin)
+#define BDC_MCPWM_TIMER_RESOLUTION_HZ 10000000                                      // 10MHz, 1 tick = 0.1us
+#define BDC_MCPWM_FREQ_HZ 25000                                                     // 25KHz PWM
+#define BDC_MCPWM_DUTY_TICK_MAX (BDC_MCPWM_TIMER_RESOLUTION_HZ / BDC_MCPWM_FREQ_HZ) // maximum value we can set for the duty cycle, in ticks
+
+Motor::Motor(int aLeftPin, int aRightPin)
 {
+    mConfig.pwm_freq_hz = 500;
+    mConfig.pwma_gpio_num = aLeftPin;
+    mConfig.pwmb_gpio_num = aRightPin;
+
+    mPwmConfig.group_id = 0;
+    mPwmConfig.resolution_hz = BDC_MCPWM_TIMER_RESOLUTION_HZ;
+
+    ESP_ERROR_CHECK(bdc_motor_new_mcpwm_device(&mConfig, &mPwmConfig, &mHandle));
 }
