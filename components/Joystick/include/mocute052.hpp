e210ca9481fc814a26d467a228f2b4d8638bb7d4
Matthew Colvin <Matthew.Colvin@garmin.com>
2023-04-25 14:24:50 -0500

    refactored mocute052 std::function setter and added some.
    Change-Id: Ic02799c95474341a8a91a47af180449375a72675

--- components/Joystick/include/mocute052.hpp
+++ components/Joystick/include/mocute052.hpp
@@ -10,13 +10,17 @@ public:
 
     Mocute052(HIDDevice aDevice);
 
-    void setJoystickHandler(std::function<void(uint8_t, uint8_t)> aJoyStickHandler) { mHandleJoystick = std::move(aJoyStickHandler); };
-
+    void onJoyStick(std::function<void(uint8_t, uint8_t)> aJoyStickHandler) { mHandleJoystick = std::move(aJoyStickHandler); };
+    void onA(std::function<void()> aPressHandler,std::function<void()> aReleaseHandler) {mHandleAPress = std::move(aPressHandler); mHandleARelease = std::move(aReleaseHandler);};
+    void onB(std::function<void()> aPressHandler,std::function<void()> aReleaseHandler) {mHandleBPress = std::move(aPressHandler); mHandleBRelease = std::move(aReleaseHandler);};
+    void onX(std::function<void()> aPressHandler,std::function<void()> aReleaseHandler) {mHandleXPress = std::move(aPressHandler); mHandleXRelease = std::move(aReleaseHandler);};
+    void onY(std::function<void()> aPressHandler,std::function<void()> aReleaseHandler) {mHandleYPress = std::move(aPressHandler); mHandleYRelease = std::move(aReleaseHandler);};
+    void onTrigger(std::function<void()> aPressHandler,std::function<void()> aReleaseHandler) {mHandleTriggerPress = std::move(aPressHandler); mHandleTriggerRelease = std::move(aReleaseHandler);};
 
 protected:
     virtual void handleInputEvent(esp_hidh_event_data_t *anInputEvent);
 
-public:
+private:
     uint8_t mX = 128;
     uint8_t mY = 128;
     std::function<void(uint8_t, uint8_t)> mHandleJoystick;
