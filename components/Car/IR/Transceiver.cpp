e31467e682bf216a4b3987a1b0ea674f16bbd276
MatthewColvin <mc067415@ohio.edu>
2023-05-04 09:15:35 -0500

    Corrected miss spelling of Transceiver Added carriers for RX and Tx Channels Added basic logic for onReceive

---
+++ components/Car/IR/Transceiver.cpp
@@ -0,0 +1,68 @@
+#include "Transceiver.hpp"
+
+#include "driver/rmt_rx.h"
+#include "driver/rmt_tx.h"
+#include "esp_log.h"
+
+#define LOG_TAG "Transceiver"
+#define EXAMPLE_IR_RESOLUTION_HZ 1000000 // 1MHz resolution, 1 tick = 1us
+
+Transceiver::Transceiver(int receivePin, int sendPin)
+{
+    setupRxChannel(receivePin);
+    setupTxChannel(sendPin);
+}
+
+void Transceiver::setupTxChannel(int txPin)
+{
+    rmt_tx_channel_config_t txConfig;
+    txConfig.clk_src = RMT_CLK_SRC_DEFAULT;
+    txConfig.mem_block_symbols = 64;
+    txConfig.resolution_hz = EXAMPLE_IR_RESOLUTION_HZ;
+    txConfig.trans_queue_depth = 4;
+    txConfig.gpio_num = txPin;
+    ESP_ERROR_CHECK(rmt_new_tx_channel(&txConfig, &mTxCh));
+
+    rmt_carrier_config_t tx_carrier_cfg;
+    tx_carrier_cfg.duty_cycle = 0.33;
+    tx_carrier_cfg.frequency_hz = 38000;
+    tx_carrier_cfg.flags.polarity_active_low = false;
+    ESP_ERROR_CHECK(rmt_apply_carrier(mTxCh, &tx_carrier_cfg));
+
+    mRxCallbacks.on_recv_done = this->onReceiveImpl;
+    ESP_ERROR_CHECK(rmt_rx_register_event_callbacks(mRxCh, &mRxCallbacks, this));
+}
+
+void Transceiver::setupRxChannel(int rxPin)
+{
+    rmt_rx_channel_config_t rxConfig;
+    rxConfig.clk_src = RMT_CLK_SRC_DEFAULT;
+    rxConfig.mem_block_symbols = 64;
+    rxConfig.resolution_hz = EXAMPLE_IR_RESOLUTION_HZ;
+    rxConfig.gpio_num = rxPin;
+    ESP_ERROR_CHECK(rmt_new_rx_channel(&rxConfig, &mRxCh));
+
+    rmt_carrier_config_t rx_carrier_cfg;
+    rx_carrier_cfg.duty_cycle = .33;
+    rx_carrier_cfg.frequency_hz = 25000;
+    rx_carrier_cfg.flags.polarity_active_low = false;
+    ESP_ERROR_CHECK(rmt_apply_carrier(mRxCh, &rx_carrier_cfg));
+}
+
+bool Transceiver::onReceiveImpl(rmt_channel_handle_t rx_chan, const rmt_rx_done_event_data_t *edata, void *user_ctx)
+{
+    return static_cast<Transceiver *>(user_ctx)->onReceive(edata);
+}
+
+bool Transceiver::onReceive(const rmt_rx_done_event_data_t *edata)
+{
+    for (int i = 0; i < edata->num_symbols; i++)
+    {
+        ESP_LOGI(LOG_TAG, "Received: %d", edata->received_symbols[i].val);
+    }
+    return false;
+}
+
+void Transceiver::send()
+{
+}
---
+++ components/Car/IR/include/Transceiver.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "driver/rmt_types.h"
+#include "driver/rmt_rx.h"
+#include "driver/rmt_tx.h"
+
+class Transceiver
+{
+public:
+    Transceiver(int recievePin, int sendPin);
+    void send();
+
+private:
+    void setupRxChannel(int rxPin);
+    void setupTxChannel(int txPin);
+
+    static bool onReceiveImpl(rmt_channel_handle_t rx_chan, const rmt_rx_done_event_data_t *edata, void *user_ctx);
+    bool onReceive(const rmt_rx_done_event_data_t *edata);
+
+    rmt_rx_event_callbacks_t mRxCallbacks;
+    rmt_tx_event_callbacks_t mTxCallbacks;
+
+    rmt_channel_handle_t mRxCh = nullptr;
+    rmt_channel_handle_t mTxCh = nullptr;
+};
