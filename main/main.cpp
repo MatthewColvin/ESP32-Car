ac8a4e2995637332218e28de29f27afaa1b974b1
MatthewColvin <mcolvin32.7@gmail.com>
2024-07-14 18:50:57 -0500

    restoring solutions main since it got accidently stomped by master merge

--- main/main.cpp
+++ main/main.cpp
@@ -22,251 +22,197 @@
 
 #define LOG_TAG "main"
 
-/*
-This Code is for testing all components that can go on a Car and its connection
-to Joystick.
+#define UpLink true           // IR Sensor
+#define TeamSyncLed true      // External LED
+#define VolcanoMeasure true   // Light Frequency Sensor & IR Sensor & Status
+#define CaveNavigation true   // Servo
+#define SampleCollection true // Servo
 
-It uses Modes to determine function this mode is shown by color of the servo.
+/* GLOBAL VARIABLES */
+// Reminder: Make your variable names descriptive
 
-
-*/
-
+// TODO: Add your additional challenge variables
+// Syntax Hint: <Class name>* <variable name>;
 Car *car;
 std::shared_ptr<controller> joystick = nullptr;
 
-class ButtonHandler {
-public:
-  virtual ~ButtonHandler(){};
-  virtual void aPress(){};
-  virtual void aRelease(){};
-  virtual void bPress(){};
-  virtual void bRelease(){};
-  virtual void xRelease(){};
-  virtual void xPress(){};
-  virtual void yRelease(){};
-  virtual void yPress(){};
-};
+#if TeamSyncLed
+LED *redLed = new LED(RedLedPin);
+LED *blueLed = new LED(BlueLedPin);
+LED *greenLed = new LED(GreenLedPin);
+#endif
 
-std::unique_ptr<ButtonHandler> currentTestHandler =
-    std::make_unique<ButtonHandler>();
+#if CaveNavigation || SampleCollection
+ServoMotor *servo = new ServoMotor(ServoPin);
+#endif
 
-class ServoHandler : public ButtonHandler {
-public:
-  ServoHandler() : mMotor(ServoPin){};
-  ~ServoHandler() { car->controlWith(joystick); }
+#if UpLink || VolcanoMeasure
+Transceiver *ir = new Transceiver(IRDETECT, IRLED);
+#endif
 
-  void aPress() override { // Control Servo With Joystick
-    mMotor.controlWith(joystick);
-  };
+#if VolcanoMeasure
+LightSensor *lightSensor = nullptr;
+#endif
 
-  void bPress() override { // Control Car With Joystick
-    car->controlWith(joystick);
-  };
+void setRGB(uint8_t aRed, uint8_t aBlue, uint8_t aGreen) {
+#if TeamSyncLed
+  redLed->setBrightness(aRed);
+  greenLed->setBrightness(aGreen);
+  blueLed->setBrightness(aBlue);
+#endif
+}
 
-  void xPress() override { mMotor.incrementAngle(10); };
-
-  void yPress() override { mMotor.decrementAngle(10); };
-
-  ServoMotor mMotor;
-};
-
-class RGBLedHandler : public ButtonHandler {
-public:
-  RGBLedHandler(LED *aRed, LED *aGreen, LED *aBlue) {
-    red = aRed;
-    green = aGreen;
-    blue = aBlue;
-
-    red->setBrightness(0);
-    green->setBrightness(0);
-    blue->setBrightness(0);
+int LEDState = 0;
+void ChangeLEDColor() {
+  switch (LEDState) {
+  case 0:
+    setRGB(0, 0, 255);
+    LEDState += 1;
+    break;
+  case 1:
+    setRGB(0, 255, 0);
+    LEDState += 1;
+    break;
+  case 2:
+    setRGB(255, 0, 0);
+    LEDState += 1;
+    break;
+  default:
+    setRGB(0, 0, 0);
+    LEDState = 0;
   }
-  LED *red;
-  LED *green;
-  LED *blue;
+}
 
-  void aPress() override {
-    red->setBrightness((red->getBrightness() + 10) % 255);
-  };
-  void bPress() override {
-    green->setBrightness((green->getBrightness() + 10) % 255);
-  };
-  void xPress() override {
-    blue->setBrightness((blue->getBrightness() + 10) % 255);
-  };
-  void yPress() override {
-    red->setBrightness(0);
-    green->setBrightness(0);
-    blue->setBrightness(0);
-  };
-};
+void sendIREasy() {
+#if UpLink
+  ir->enableTx();
+  ir->send(0, 0);
+  ir->disableTx();
+#endif
+}
 
-class LightSensorHandler : public ButtonHandler {
-public:
-  LightSensorHandler()
-      : mLightSensor(LightSensorPin, [this]() { pulseCount++; }) {}
-
-  void aPress() override {
-    mLightSensor.Enable();
-    pulseCount = 0;
-  };
-
-  void bPress() override { mLightSensor.Disable(); };
-
-  void xPress() override { // Print Collection results
-    int timeBetweenPulses = 0;
-    if (pulseCount > 0) {
-      timeBetweenPulses = mLightSensor.GetCollectionTime() / pulseCount;
-    }
-    ESP_LOGI(LOG_TAG, "TimeMeasured: %lli NumPulses: %d AvgTimeBetween: %d",
-             mLightSensor.GetCollectionTime(), pulseCount, timeBetweenPulses);
+// DEV_TODO Add Address for Operation data
+#define TowerAddress 0x00
+void sendIRHard(uint16_t op, uint16_t aInputValue) {
+// DEV_TODO Add Operations
+#if (UpLink)
+  uint16_t answer = 0;
+  switch (op) {
+  case 1:
+    answer = aInputValue;
+    break;
+  case 2:
+    answer = aInputValue;
+    break;
+  case 3:
+    answer = aInputValue;
+    break;
+  case 4:
+    answer = aInputValue;
+    break;
+  case 5:
+    answer = aInputValue;
+    break;
+  default:
+    answer = aInputValue;
   }
 
-  LightSensor mLightSensor;
+  ir->enableTx();
+  ir->send(TowerAddress, answer);
+  ir->disableTx();
+#endif
+}
 
-  // Light Sensor Code
-  int pulseCount = 0;
-};
+void controlServoWithJoystick() {
+#if CaveNavigation || SampleCollection
+  servo->controlWith(joystick);
+#endif
+}
 
-class IRHandler : public ButtonHandler {
-public:
-  IRHandler() : mIr(IRDETECT, IRLED) {
-    mIr.mSetReceiveHandler([](auto addr, auto cmd, auto isRepeat) {
-      ESP_LOGI(LOG_TAG, "Received Address:%i Command:%i \n", addr, cmd);
-    });
-    mIr.disableRx();
-    mIr.disableTx();
+void controlCarWithJoystick() { car->controlWith(joystick); }
+
+int numPulses = 0;
+void startVolcanoCollection() {
+#if VolcanoMeasure
+  numPulses = 0;
+  lightSensor->Enable();
+#endif
+}
+
+void onPulse() { numPulses++; }
+
+uint16_t timeBetweenPulses = 0;
+void endVolcanoCollection() {
+#if VolcanoMeasure
+  lightSensor->Disable();
+  if (numPulses > 0) {
+    timeBetweenPulses = lightSensor->GetCollectionTime() / numPulses;
   }
-  void aPress() override { // Toggle Receive
-    mRxEnabled ? mIr.disableRx() : mIr.enableRx();
-    mRxEnabled = !mRxEnabled;
-  };
-  void bPress() override { // Toggle Send
-    mTxEnabled ? mIr.disableTx() : mIr.enableTx();
-    mTxEnabled = !mTxEnabled;
-  };
+#endif
+}
 
-  void xPress() override { mIr.send(0, 0); };
-  void yPress() override { mIr.send(0, 135); };
+// DEV_TODO Update Address for Volcano Data
+#define VolcanoDataTowerAddress 0x00
+void sendVolcanoTimeBetweenPulses() {
+#if VolcanoMeasure
+  ir->enableTx();
+  ir->send(VolcanoDataTowerAddress, timeBetweenPulses);
+  ir->disableTx();
+  ESP_LOGI(LOG_TAG, "timeBetweenPulses: %d sent", timeBetweenPulses);
+#endif
+}
 
-  Transceiver mIr;
-  bool mRxEnabled = false;
-  bool mTxEnabled = false;
-};
+/* JOYSTICK CALLBACKS */
+void onAPress() { ChangeLEDColor(); };
+void onARelease() { sendIREasy(); };
+void onBPress() { startVolcanoCollection(); };
+void onBRelease() { endVolcanoCollection(); };
+void onXPress() { sendVolcanoTimeBetweenPulses(); };
+void onXRelease(){};
+void onYPress(){};
+void onYRelease(){};
+void onTriggerPress() { controlServoWithJoystick(); };
+void onTriggerRelease() { controlCarWithJoystick(); };
 
-class DriveHandler : public ButtonHandler {
-public:
-  bool mTurboEnabled = false;
+void onReceiveIRData(uint16_t address, uint16_t data, bool isRepeat) {
+  ESP_LOGI("MAIN", "Address=%04X, Command=%04X\r\n\r\n", address, data);
+  sendIRHard(address, data);
+}
 
-  void aPress() override { // Toggle Turbo
-    mTurboEnabled ? car->disableTurbo() : car->enableTurbo();
-    mTurboEnabled = !mTurboEnabled;
-  };
-
-  void bPress() override { // Increase Cruise Speed
-    car->setCruiseSpeed(car->getCruiseSpeed() + 1000);
-  };
-
-  void xPress() override { // decrease Cruise Speed
-    car->setCruiseSpeed(car->getCruiseSpeed() - 1000);
-  };
-
-  void yPress() override { // Change Handling
-    car->getHandling() == Car::Handling::Car
-        ? car->setHandling(Car::Handling::Tank)
-        : car->setHandling(Car::Handling::Car);
-  };
-};
-
-class ButtonHandlerFactory {
-public:
-  ButtonHandlerFactory()
-      : intRed(InternalRedLedPin, true), intGreen(InternalGreenLedPin, true),
-        intBlue(InternalBlueLedPin, true), extRed(RedLedPin),
-        extGreen(GreenLedPin), extBlue(BlueLedPin) {}
-  virtual ~ButtonHandlerFactory() = default;
-
-  enum class sensorType {
-    Servo,
-    ExternalLed,
-    InternalLed,
-    LightSensor,
-    IR,
-    Driving,
-    COUNT
-  };
-  std::unique_ptr<ButtonHandler> GetHandler(sensorType aSensor) {
-    switch (aSensor) {
-    case sensorType::Servo:
-      ESP_LOGI(LOG_TAG, "---Servo TEST---");
-      return std::make_unique<ServoHandler>();
-    case sensorType::LightSensor:
-      ESP_LOGI(LOG_TAG, "---Light Sensor TEST---");
-      return std::make_unique<LightSensorHandler>();
-    case sensorType::IR:
-      ESP_LOGI(LOG_TAG, "---IR TEST---");
-      return std::make_unique<IRHandler>();
-    case sensorType::InternalLed:
-      ESP_LOGI(LOG_TAG, "---Internal LED TEST---");
-      return std::make_unique<RGBLedHandler>(&intRed, &intGreen, &intBlue);
-    case sensorType::ExternalLed:
-      ESP_LOGI(LOG_TAG, "---External LED TEST---");
-      return std::make_unique<RGBLedHandler>(&extRed, &extGreen, &extBlue);
-    case sensorType::Driving:
-      ESP_LOGI(LOG_TAG, "---Driving TEST---");
-      return std::make_unique<DriveHandler>();
-    default:
-      return std::make_unique<ButtonHandler>();
-    };
-  };
-
-  std::unique_ptr<ButtonHandler> GetNextHandler() {
-    sensorType nextType =
-        static_cast<sensorType>((static_cast<int>(mCurrentType) + 1) %
-                                static_cast<int>(sensorType::COUNT));
-    mCurrentType = nextType;
-    return GetHandler(nextType);
-  };
-
-  LED intRed;
-  LED intGreen;
-  LED intBlue;
-
-  LED extRed;
-  LED extGreen;
-  LED extBlue;
-
-  sensorType mCurrentType = sensorType::COUNT;
-};
-
-ButtonHandlerFactory handlerFactory;
-void onTriggerPress() { currentTestHandler = handlerFactory.GetNextHandler(); };
-void onTriggerRelease(){};
+void registerJoystickButtonHandlers(std::shared_ptr<Mocute052> aJoystick) {
+  aJoystick->onA(onAPress, onARelease);
+  aJoystick->onB(onBPress, onBRelease);
+  aJoystick->onX(onXPress, onXRelease);
+  aJoystick->onY(onYPress, onYRelease);
+  aJoystick->onTrigger(onTriggerPress, onTriggerRelease);
+}
 
 extern "C" void app_main(void) {
   nvs_flash_init();
   auto bt = BTClassicHID::getInstance();
 
-  // Put in your MAC Address
-  // Note 0x00 Acts as a don't care this can allow for connection to multiple
-  // remotes. esp_bd_addr_t joystickAddress{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-  esp_bd_addr_t joystickAddress{0xD4, 0x14, 0xA7, 0x00, 0x00, 0x00};
+  // TODO: Put in your MAC Address
+  // esp_bd_addr_t joystickAddress{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+  esp_bd_addr_t joystickAddress{0xD4, 0x14, 0xA7, 0x0C, 0x14, 0x00};
   joystick = bt->connect<controller>(joystickAddress);
 
+  // TODO: Create the new motors for your car
   Motor *left = new Motor(LeftMotorLeftPin, LeftMotorRightPin);
   Motor *right = new Motor(RightMotorLeftPin, RightMotorRightPin);
+
+  // TODO: using the joystick and motor variables make a new car.
   car = new Car(joystick, left, right);
   car->enableTurbo();
 
-  joystick->onA([] { currentTestHandler->aPress(); },
-                [] { currentTestHandler->aRelease(); });
-  joystick->onB([] { currentTestHandler->bPress(); },
-                [] { currentTestHandler->bRelease(); });
-  joystick->onX([] { currentTestHandler->xPress(); },
-                [] { currentTestHandler->xRelease(); });
-  joystick->onY([] { currentTestHandler->yPress(); },
-                [] { currentTestHandler->yRelease(); });
+#if VolcanoMeasure
+  lightSensor = new LightSensor(LightSensorPin, onPulse);
+#endif
 
-  joystick->onTrigger(onTriggerPress, onTriggerRelease);
+// TODO add log to remind to enable RX
+#if UpLink
+  ir->mSetReceiveHandler(onReceiveIRData);
+#endif
+
+  registerJoystickButtonHandlers(joystick);
   vTaskDelete(NULL); // Delete Main Task
 }
