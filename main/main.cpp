fdedaa701a997ecf125dc120eb55d08864baf1f1
Matthew Colvin <Matthew.Colvin@garmin.com>
2024-07-15 11:21:56 -0500

    Added template for 2024
    Change-Id: Id687fdada8c4b40a78de5fe06a3bfa4c65f2a4b6

--- main/main.cpp
+++ main/main.cpp
@@ -23,250 +23,115 @@
 #define LOG_TAG "main"
 
 /*
-This Code is for testing all components that can go on a Car and its connection
-to Joystick.
+  GLOBAL VARIABLES
 
-It uses Modes to determine function this mode is shown by color of the servo.
-
-
+  Tip: nullptr means we currently have no object stored in that variable.
 */
 
-Car *car;
+Car *car = nullptr;
+Motor *left = nullptr;
+Motor *right = nullptr;
 std::shared_ptr<controller> joystick = nullptr;
 
-class ButtonHandler {
-public:
-  virtual ~ButtonHandler(){};
-  virtual void aPress(){};
-  virtual void aRelease(){};
-  virtual void bPress(){};
-  virtual void bRelease(){};
-  virtual void xRelease(){};
-  virtual void xPress(){};
-  virtual void yRelease(){};
-  virtual void yPress(){};
-};
+ServoMotor *servo = nullptr;
+LED* red = nullptr;
+LED* green = nullptr;
+LED* blue = nullptr;
 
-std::unique_ptr<ButtonHandler> currentTestHandler =
-    std::make_unique<ButtonHandler>();
+// Use Area below to make the variable for your sensors similar to above examples
+// <variableType> *<variableName> = nullptr;
 
-class ServoHandler : public ButtonHandler {
-public:
-  ServoHandler() : mMotor(ServoPin){};
-  ~ServoHandler() { car->controlWith(joystick); }
+///////////////////////////////////////////////////////////////////
 
-  void aPress() override { // Control Servo With Joystick
-    mMotor.controlWith(joystick);
-  };
 
-  void bPress() override { // Control Car With Joystick
-    car->controlWith(joystick);
-  };
+/*
+  TODO: Code your car to do things when you press buttons.
 
-  void xPress() override { mMotor.incrementAngle(10); };
+  Tip: You can use one button for more than one action
+  by checking if other buttons are held down by using the
+  joystick variable.See code below
 
-  void yPress() override { mMotor.decrementAngle(10); };
-
-  ServoMotor mMotor;
-};
-
-class RGBLedHandler : public ButtonHandler {
-public:
-  RGBLedHandler(LED *aRed, LED *aGreen, LED *aBlue) {
-    red = aRed;
-    green = aGreen;
-    blue = aBlue;
-
-    red->setBrightness(0);
-    green->setBrightness(0);
-    blue->setBrightness(0);
+  if (joystick->isTriggerDown()){
+    // Do something with trigger down
+  }else{
+    // Do other thing when trigger is up
   }
-  LED *red;
-  LED *green;
-  LED *blue;
+*/
 
-  void aPress() override {
-    red->setBrightness((red->getBrightness() + 10) % 255);
-  };
-  void bPress() override {
-    green->setBrightness((green->getBrightness() + 10) % 255);
-  };
-  void xPress() override {
-    blue->setBrightness((blue->getBrightness() + 10) % 255);
-  };
-  void yPress() override {
-    red->setBrightness(0);
-    green->setBrightness(0);
-    blue->setBrightness(0);
-  };
-};
+void onAPress(){
+  /* This will run when you press the A button down */
 
-class LightSensorHandler : public ButtonHandler {
-public:
-  LightSensorHandler()
-      : mLightSensor(LightSensorPin, [this]() { pulseCount++; }) {}
+}
 
-  void aPress() override {
-    mLightSensor.Enable();
-    pulseCount = 0;
-  };
+void onARelease(){
+  /*This will run when you release the A button*/
+}
 
-  void bPress() override { mLightSensor.Disable(); };
+void onBPress(){
+  // Tip: try typing <variableName>-> and then wait for editor to show you what you can do.
+  // Lets say you want to make your red light come on but don't know how...
 
-  void xPress() override { // Print Collection results
-    int timeBetweenPulses = 0;
-    if (pulseCount > 0) {
-      timeBetweenPulses = mLightSensor.GetCollectionTime() / pulseCount;
-    }
-    ESP_LOGI(LOG_TAG, "TimeMeasured: %lli NumPulses: %d AvgTimeBetween: %d",
-             mLightSensor.GetCollectionTime(), pulseCount, timeBetweenPulses);
-  }
+  // Try typing red->
+  // The editor will show you all the things that you can do with that variable.
+  // in that list you see setBrightness so click it to auto fill
+  // red->setBrightness
+  // Now add a parenthesis to see what the function takes
+  // red->setBrightness(
+  // You will notice a window pop up and see it takes an int which is just a number so fill it in
+  // red->setBrightness(50)
+  // Dont forget to add your semicolon
+  // red->setBrightness(50);
+  // This is a full working line of code that will turn on your red LED.
+  // Maybe you could try to move your servo next?
+}
+void onBRelease(){}
 
-  LightSensor mLightSensor;
+void onXPress(){}
+void onXRelease(){}
 
-  // Light Sensor Code
-  int pulseCount = 0;
-};
+void onYPress(){}
+void onYRelease(){}
 
-class IRHandler : public ButtonHandler {
-public:
-  IRHandler() : mIr(IRDETECT, IRLED) {
-    mIr.mSetReceiveHandler([](auto addr, auto cmd, auto isRepeat) {
-      ESP_LOGI(LOG_TAG, "Received Address:%i Command:%i \n", addr, cmd);
-    });
-    mIr.disableRx();
-    mIr.disableTx();
-  }
-  void aPress() override { // Toggle Receive
-    mRxEnabled ? mIr.disableRx() : mIr.enableRx();
-    mRxEnabled = !mRxEnabled;
-  };
-  void bPress() override { // Toggle Send
-    mTxEnabled ? mIr.disableTx() : mIr.enableTx();
-    mTxEnabled = !mTxEnabled;
-  };
-
-  void xPress() override { mIr.send(0, 0); };
-  void yPress() override { mIr.send(0, 135); };
-
-  Transceiver mIr;
-  bool mRxEnabled = false;
-  bool mTxEnabled = false;
-};
-
-class DriveHandler : public ButtonHandler {
-public:
-  bool mTurboEnabled = false;
-
-  void aPress() override { // Toggle Turbo
-    mTurboEnabled ? car->disableTurbo() : car->enableTurbo();
-    mTurboEnabled = !mTurboEnabled;
-  };
-
-  void bPress() override { // Increase Cruise Speed
-    car->setCruiseSpeed(car->getCruiseSpeed() + 1000);
-  };
-
-  void xPress() override { // decrease Cruise Speed
-    car->setCruiseSpeed(car->getCruiseSpeed() - 1000);
-  };
-
-  void yPress() override { // Change Handling
-    car->getHandling() == Car::Handling::Car
-        ? car->setHandling(Car::Handling::Tank)
-        : car->setHandling(Car::Handling::Car);
-  };
-};
-
-class ButtonHandlerFactory {
-public:
-  ButtonHandlerFactory()
-      : intRed(InternalRedLedPin, true), intGreen(InternalGreenLedPin, true),
-        intBlue(InternalBlueLedPin, true), extRed(RedLedPin),
-        extGreen(GreenLedPin), extBlue(BlueLedPin) {}
-  virtual ~ButtonHandlerFactory() = default;
-
-  enum class sensorType {
-    Servo,
-    ExternalLed,
-    InternalLed,
-    LightSensor,
-    IR,
-    Driving,
-    COUNT
-  };
-  std::unique_ptr<ButtonHandler> GetHandler(sensorType aSensor) {
-    switch (aSensor) {
-    case sensorType::Servo:
-      ESP_LOGI(LOG_TAG, "---Servo TEST---");
-      return std::make_unique<ServoHandler>();
-    case sensorType::LightSensor:
-      ESP_LOGI(LOG_TAG, "---Light Sensor TEST---");
-      return std::make_unique<LightSensorHandler>();
-    case sensorType::IR:
-      ESP_LOGI(LOG_TAG, "---IR TEST---");
-      return std::make_unique<IRHandler>();
-    case sensorType::InternalLed:
-      ESP_LOGI(LOG_TAG, "---Internal LED TEST---");
-      return std::make_unique<RGBLedHandler>(&intRed, &intGreen, &intBlue);
-    case sensorType::ExternalLed:
-      ESP_LOGI(LOG_TAG, "---External LED TEST---");
-      return std::make_unique<RGBLedHandler>(&extRed, &extGreen, &extBlue);
-    case sensorType::Driving:
-      ESP_LOGI(LOG_TAG, "---Driving TEST---");
-      return std::make_unique<DriveHandler>();
-    default:
-      return std::make_unique<ButtonHandler>();
-    };
-  };
-
-  std::unique_ptr<ButtonHandler> GetNextHandler() {
-    sensorType nextType =
-        static_cast<sensorType>((static_cast<int>(mCurrentType) + 1) %
-                                static_cast<int>(sensorType::COUNT));
-    mCurrentType = nextType;
-    return GetHandler(nextType);
-  };
-
-  LED intRed;
-  LED intGreen;
-  LED intBlue;
-
-  LED extRed;
-  LED extGreen;
-  LED extBlue;
-
-  sensorType mCurrentType = sensorType::COUNT;
-};
-
-ButtonHandlerFactory handlerFactory;
-void onTriggerPress() { currentTestHandler = handlerFactory.GetNextHandler(); };
+void onTriggerPress() {};
 void onTriggerRelease(){};
+
+void postCarInitSetup(){
+    servo = new ServoMotor(ServoPin);
+    red = new LED(RedLedPin);
+    green = new LED(GreenLedPin);
+    blue = new LED(BlueLedPin);
+
+    if(car){
+      // TODO: Maybe you could update this line to make the car faster?
+      car->setCruiseSpeed(Motor::MAX_SPEED - 5000);
+    }
+    if (joystick){
+      joystick->onA(onAPress,onARelease);
+      joystick->onB(onBPress,onBRelease);
+      joystick->onX(onXPress,onXRelease);
+      joystick->onY(onYPress,onYRelease);
+      joystick->onTrigger(onTriggerPress, onTriggerRelease);
+    }
+}
 
 extern "C" void app_main(void) {
   nvs_flash_init();
   auto bt = BTClassicHID::getInstance();
 
-  // Put in your MAC Address
-  // Note 0x00 Acts as a don't care this can allow for connection to multiple
-  // remotes. esp_bd_addr_t joystickAddress{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-  esp_bd_addr_t joystickAddress{0xD4, 0x14, 0xA7, 0x00, 0x00, 0x00};
+  // TODO: Put in your MAC Address
+  esp_bd_addr_t joystickAddress{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
   joystick = bt->connect<controller>(joystickAddress);
 
-  Motor *left = new Motor(LeftMotorLeftPin, LeftMotorRightPin);
-  Motor *right = new Motor(RightMotorLeftPin, RightMotorRightPin);
-  car = new Car(joystick, left, right);
-  car->enableTurbo();
+  /*
+   The motor variables for your car have already been set below use
+   them to remember the format of setting a variable
+  */
+  left = new Motor(LeftMotorLeftPin, LeftMotorRightPin);
+  right = new Motor(RightMotorLeftPin, RightMotorRightPin);
 
-  joystick->onA([] { currentTestHandler->aPress(); },
-                [] { currentTestHandler->aRelease(); });
-  joystick->onB([] { currentTestHandler->bPress(); },
-                [] { currentTestHandler->bRelease(); });
-  joystick->onX([] { currentTestHandler->xPress(); },
-                [] { currentTestHandler->xRelease(); });
-  joystick->onY([] { currentTestHandler->yPress(); },
-                [] { currentTestHandler->yRelease(); });
+  // TODO: Set your Car Variable to enable driving the car
+  // Format: <VariableName> = new <VariableType>(arg1,arg2...);
 
-  joystick->onTrigger(onTriggerPress, onTriggerRelease);
+  postCarInitSetup();
   vTaskDelete(NULL); // Delete Main Task
 }
